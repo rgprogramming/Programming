7baebb2217dc3ecd4fb0cc728c09fb716d56076d
rgprogramming <mitalcs@gmail.com>
2015-03-29 23:47:52 -0700

    Initial Commit

---
+++ src/main/java/CoreJava/HelloWorld.java
@@ -0,0 +1,10 @@
+package CoreJava;
+
+/**
+ * Created by mital on 3/29/15.
+ */
+public class HelloWorld {
+    public static void main( String... args ) {
+        System.out.println("Hello there...");
+    }
+}
